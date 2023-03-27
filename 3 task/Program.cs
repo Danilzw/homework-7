4390a2034bc7526a4d2a455ca6e7323bba5ebf11
Danil <shwedx@Yandex.ru>
2023-03-27 16:27:35 +0300

    done

---
+++ 3 task/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+int [,] array = new int [3,3];
+
+for(int i=0;i<array.GetLength(0);i++)
+{
+    for(int j=0;j<array.GetLength(1);j++)
+    {
+        array[i,j] = new Random().Next(1,9);
+        System.Console.Write(array[i,j] + " ");
+    }
+    System.Console.WriteLine();
+}
+
+double first = 0;
+
+for(int i=0;i<array.GetLength(1);i++)
+{
+    for(int j=0;j<array.GetLength(0);j++)
+    {
+       first = first + array[i,j];
+    }
+    double result = first / array.GetLength(0);
+    System.Console.Write(result + " ");
+}
+
+
+
